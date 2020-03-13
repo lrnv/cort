6e87fadbe3a7b4f55174cf3ea2ed2006e508def3
Oskar Laverny <oskar.laverny@gmail.com>
2020-03-13 17:23:55 +0100

    modifying the tests

--- tests/testthat/test-utils-pipe.R
+++ tests/testthat/test-utils-pipe.R
@@ -1,3 +1,14 @@
+library(cort)
+
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
+})
+
+
+test_that("should be fine", {
+  expect_equal(4,4)
+})
+
+test_that("pipe works correctly", {
+  expect_equal(c(1,2) %>% sum,3)
 })
