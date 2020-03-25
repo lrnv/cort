a8ef2f569b975554329ed17214e3fe2fcb20877c
Oskar Laverny <oskar.laverny@gmail.com>
2020-03-25 10:39:06 +0100

    adding bootstrap; other chnages

--- R/WeightedBox.R
+++ R/WeightedBox.R
@@ -20,7 +20,6 @@ NULL
 
 WeightedBox = function(box,data,weight=1,split_dims=1:box@dim,min_node_size = 1) {
 
-
   # checks :
   if(ncol(data) != box@dim){ stop ("The data shuold have the same umber of colums as the dimension of the box.")}
   if(!all(contains(box,data))){ stop("The box should contain the data")}
@@ -57,7 +56,7 @@ setMethod(f = "is_splittable",
 
 setMethod(f="split",
           signature = c(object="WeightedBox"),
-          definition = function(object,p_val_threshold = 0.75, number_max_dim=object@dim,verbose=FALSE){
+          definition = function(object,p_val_threshold = 0.75, number_max_dim=object@dim,verbose_lvl=1){
             if(!is_splittable(object)){return(list(object))}
 
             # Randomize splitting dimensions :
@@ -72,15 +71,27 @@ setMethod(f="split",
               non_taken_dims = numeric()
             }
 
+
+
             if (is_splittable(object)){ #if it is splittable"
               optimizer = Optmize_breakpoint(object@data[,object@split_dims],
                                              object@a[object@split_dims],
                                              object@b[object@split_dims],
-                                             verbose=verbose)
+                                             verbose_lvl=verbose_lvl-1)
 
 
               bp = optimizer$bp # for the moment we split in the middle.
               p_values = optimizer$p_values
+
+              # if any of the breakpoints are touching the boundary, we remove the dimensions :
+              touches_boundary = (((bp <= object@a[object@split_dims])+(bp >= object@b[object@split_dims])))>0
+              if(any(touches_boundary)){
+                if(verbose_lvl>0){print("Be carrefull, we are removing a splitting dimensions because breakpoint touches the boundary. ")}
+                p_values[which(touches_boundary)] = p_val_threshold+1
+              }
+
+
+
 
               # try to remove some pslitting dimensions :
               to_be_removed = p_values > p_val_threshold
@@ -94,6 +105,11 @@ setMethod(f="split",
                   bp = bp[!to_be_removed]
                 }
 
+                if(length(object@split_dims) == 1){
+                  object@split_dims = c(object@split_dims,non_taken_dims)
+                  return(list(object))
+                }
+
 
                 # Compute new boxes and assign the data to each box :
                 new_boxes = split(as(object,"Box"),bp,object@split_dims)
@@ -101,7 +117,7 @@ setMethod(f="split",
                 data_without_bp = object@data[!are_the_breakpoint,]
 
                 new_boxes = purrr::map(new_boxes,function(b){
-                  dat = object@data[contains(b,data_without_bp,type="loose"),,drop=FALSE]
+                  dat = object@data[contains(b,data_without_bp,type="rllc"),,drop=FALSE]
                   WeightedBox(box = b, data = dat,
                               weight =object@weight * nrow(dat)/nrow(object@data),
                               split_dims = c(object@split_dims,non_taken_dims),
@@ -115,7 +131,7 @@ setMethod(f="split",
             return(list(object))
           })
 
-Optmize_breakpoint <- function(data,a=0,b=1,verbose=FALSE){
+Optmize_breakpoint <- function(data,a=0,b=1,verbose_lvl=0){
 
   # Compute prerequisites :
   n = nrow(data)
@@ -137,7 +153,8 @@ Optmize_breakpoint <- function(data,a=0,b=1,verbose=FALSE){
     min = bp*binary_repr
     max = bp^(1-binary_repr)
     f = colMeans(colSums((min <= z_rep)&(z_rep<max))==d)
-    loss = - sum( (f^2) / apply(max[,1,]-min[,1,],2,prod) )
+    xxx = (f^2) / apply(max[,1,]-min[,1,],2,prod)
+    loss = - sum( xxx[!is.na(xxx)] ) # permet de ne pas avoir de bug quand il y a des boites de taille 0.
     return(loss)
   }
 
@@ -162,15 +179,15 @@ Optmize_breakpoint <- function(data,a=0,b=1,verbose=FALSE){
 
   # Return the breakpoint (re-normalized to the box) :
   bp = a + optimizer$par*(b-a)
-  if(verbose) cat("           breakpoints :",bp,"\n")
+  if(verbose_lvl>0) cat("           breakpoints :",bp,"\n")
 
   # Compute the p_values :
   p_values = compute_bootstrapped_p_values(z,bp)
-  if (verbose) cat("           p_values    :",p_values,"\n\n")
+  if (verbose_lvl>0) cat("           p_values    :",p_values,"\n\n")
   return(list(bp=bp,p_values=p_values))
 }
 
-compute_bootstrapped_p_values <- function(z,bp,N = 999){
+compute_bootstrapped_p_values <- function(z,bp,N = 499){
 
   # prerequisites :
   d = nrow(z)
@@ -179,7 +196,6 @@ compute_bootstrapped_p_values <- function(z,bp,N = 999){
   binary_repr = t(sapply(1:(2^d),number2binary,d))
   min = bp*t(binary_repr)
   max = bp^t(1-binary_repr)
-
 
   lambda_l = apply(max-min,2,prod) # 2^d
   lambda_k = vapply(1:d,function(d_rem){lambda_l/((max-min)[d_rem,])},lambda_l) # 2^d * d
@@ -192,40 +208,36 @@ compute_bootstrapped_p_values <- function(z,bp,N = 999){
 
   statistic <- sum(f_l^2/lambda_l) -2 * colSums(f_k * f_l / lambda_k) # d
 
-
-
-  # Construct bootstrapped core:
-  z_boot = z
-  dim(z_boot) = c(d,n,1,1,1)
-  z_boot = z_boot[,,rep(1,N),,rep(1,d),drop=FALSE]
-
-  index = diag(d)==1
+  # NOW BOOTRAP THE SAME THING by first augmenting the dimensionality of z, min, max. The issue is that it eats a bunch of memory an calls GC a lot.
+  # maybe this should go in C.
+  index = (diag(d)==1)
   dim(index) = c(d,d,1,1,1)
   index = aperm(index,c(1,3,4,5,2))
-  z_boot[index[,rep(1,n),rep(1,N),,]] <- runif(n*N*d)
-  z_boot = z_boot[,,,rep(1,2^d),]
 
-  min_boot = min
-  dim(min_boot) = c(d,2^d,1,1,1)
-  min_boot = aperm(min_boot,c(1,3,4,2,5))
-  min_boot = min_boot[,rep(1,n),rep(1,N),,rep(1,d)]
+  dim(z) = c(d,n,1,1,1)
+  z = z[,,rep(1,N),,rep(1,d),drop=FALSE]
+  z[index[,rep(1,n),rep(1,N),,]] <- runif(n*N*d) # the bootstrap is here.
+  z = z[,,,rep(1,2^d),] # TAKES TIME 5
 
-  max_boot = max
-  dim(max_boot) = c(d,2^d,1,1,1)
-  max_boot = aperm(max_boot,c(1,3,4,2,5))
-  max_boot = max_boot[,rep(1,n),rep(1,N),,rep(1,d)]
+  dim(min) = c(d,2^d,1,1,1)
+  min = aperm(min,c(1,3,4,2,5))
+  min = min[,rep(1,n),rep(1,N),,rep(1,d)] # TAKES TIME 5
 
-  core_boot = (min_boot <= z_boot) & (max_boot > z_boot) # d, n, N, 2^d, d
+  dim(max) = c(d,2^d,1,1,1)
+  max = aperm(max,c(1,3,4,2,5))
+  max = max[,rep(1,n),rep(1,N),,rep(1,d)] # TAKES TIME 5
+
+  core_boot = (min <= z) & (max > z) # d, n, N, 2^d, d # TAKES TIME 5
 
   # Compute bootstrapped_statistic :
-  f_l_boot = colMeans(colSums(core_boot,dims=1) == d)
+  f_l_boot = colMeans(colSums(core_boot,dims=1) == d) # TAKES TIME 1
 
   f_k_boot = vapply(1:d,function(d_rem){
-    colMeans(colSums(core_boot[-d_rem,,,,d_rem,drop=FALSE],dims=1)==(d-1))
+    colMeans(colSums(core_boot[-d_rem,,,,d_rem,drop=FALSE],dims=1)==(d-1)) # TAKES TIME 5
   },array(0.,c(N,2^d))) #(N,2^d,d)
 
   # Summarise :
-  bootstrap_samples = apply(aperm(f_l_boot^2,c(2,3,1))/lambda_l - 2 * aperm(f_k_boot*f_l_boot,c(2,3,1))/vapply(1:N,function(i){lambda_k},lambda_k),c(2,3),sum)
+  bootstrap_samples = apply(aperm(f_l_boot^2,c(2,3,1))/lambda_l - 2 * aperm(f_k_boot*f_l_boot,c(2,3,1))/vapply(1:N,function(i){lambda_k},lambda_k),c(2,3),sum) # TAKES TIME 1
   p_val = rowMeans(statistic < bootstrap_samples)
   return(p_val)
 }
@@ -234,7 +246,60 @@ compute_bootstrapped_p_values <- function(z,bp,N = 999){
 
 
 
+compute_bootstrapped_p_values_with_rray <- function(z,bp,N = 499){
 
+  # prerequisites :
+  d = nrow(z)
+  n = ncol(z)
+
+  binary_repr = t(sapply(1:(2^d),number2binary,d))
+  min = bp*t(binary_repr)
+  max = bp^t(1-binary_repr)
+
+  lambda_l = apply(max-min,2,prod) # 2^d
+  lambda_k = vapply(1:d,function(d_rem){lambda_l/((max-min)[d_rem,])},lambda_l) # 2^d * d
+
+  # Compute empirical value of the statistic :
+  core = vapply(1:2^d,function(.x){((min[,.x]<=z)*(max[,.x]>z))==1},FUN.VALUE = z) # dims : d, n, 2^d
+
+  f_l = colMeans(colSums(core,dims=1)==d) # 2^d
+  f_k = vapply(1:d,function(d_rem){ colMeans(colSums(core[-d_rem,,,drop=FALSE],dims=1)==d-1)},f_l) # 2^d, d
+
+  statistic <- sum(f_l^2/lambda_l) -2 * colSums(f_k * f_l / lambda_k) # d
+
+  # NOW BOOTRAP THE SAME THING by first augmenting the dimensionality of z, min, max. The issue is that it eats a bunch of memory an calls GC a lot.
+  # maybe this should go in C.
+  index = (diag(d)==1)
+  dim(index) = c(d,d,1,1,1)
+  index = aperm(index,c(1,3,4,5,2))
+
+  dim(z) = c(d,n,1,1,1)
+  z = z[,,rep(1,N),,rep(1,d),drop=FALSE]
+  z[index[,rep(1,n),rep(1,N),,]] <- runif(n*N*d) # the bootstrap is here.
+  z = z[,,,rep(1,2^d),] # TAKES TIME 5
+
+  dim(min) = c(d,2^d,1,1,1)
+  min = aperm(min,c(1,3,4,2,5))
+  min = min[,rep(1,n),rep(1,N),,rep(1,d)] # TAKES TIME 5
+
+  dim(max) = c(d,2^d,1,1,1)
+  max = aperm(max,c(1,3,4,2,5))
+  max = max[,rep(1,n),rep(1,N),,rep(1,d)] # TAKES TIME 5
+
+  core_boot = (min <= z) & (max > z) # d, n, N, 2^d, d # TAKES TIME 5
+
+  # Compute bootstrapped_statistic :
+  f_l_boot = colMeans(colSums(core_boot,dims=1) == d) # TAKES TIME 1
+
+  f_k_boot = vapply(1:d,function(d_rem){
+    colMeans(colSums(core_boot[-d_rem,,,,d_rem,drop=FALSE],dims=1)==(d-1)) # TAKES TIME 5
+  },array(0.,c(N,2^d))) #(N,2^d,d)
+
+  # Summarise :
+  bootstrap_samples = apply(aperm(f_l_boot^2,c(2,3,1))/lambda_l - 2 * aperm(f_k_boot*f_l_boot,c(2,3,1))/vapply(1:N,function(i){lambda_k},lambda_k),c(2,3),sum) # TAKES TIME 1
+  p_val = rowMeans(statistic < bootstrap_samples)
+  return(p_val)
+}
 
 
 
